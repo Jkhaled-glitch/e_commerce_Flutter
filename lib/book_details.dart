1b9c03cb11fddff10c9145ff3cc8004df97417cd
Jouablia Khaled <khaledjb584@gmail.com>
2023-03-07 10:43:50 +0100

    reorganize project

--- lib/book_details.dart
+++ lib/book_details.dart
@@ -1,29 +1,76 @@
 import 'package:flutter/material.dart';
 
-class BookDetails extends StatelessWidget {
+class BookDetails extends StatefulWidget {
   final String title;
   final String image;
   final double price;
 
-  BookDetails({required this.title, required this.image, required this.price});
+  const BookDetails({Key? key, required this.title, required this.image, required this.price})
+      : super(key: key);
+
+  @override
+  _BookDetailsState createState() => _BookDetailsState();
+}
+
+class _BookDetailsState extends State<BookDetails> {
+  int _quantity = 1;
+
+  void _addToCart() {
+    // Ajouter le contenu dans le panier avec la quantité sélectionnée
+    print('Ajouté au panier: ${widget.title}, $_quantity, ${widget.price * _quantity}\$');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Image.network(image),
-          SizedBox(height: 16.0),
-          Text(title, style: TextStyle(fontSize: 24.0)),
-          SizedBox(height: 16.0),
-          Text("\$${price.toStringAsFixed(2)}",
-              style: TextStyle(fontSize: 20.0)),
+          Image.asset(widget.image),
+          SizedBox(height: 20),
+          Text(
+            widget.title,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "\$${widget.price}",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Quantité:'),
+              SizedBox(width: 10),
+              Container(
+                width: 50,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    setState(() {
+                      _quantity = int.tryParse(value) ?? 1;
+                    });
+                  },
+                  controller: TextEditingController(text: '1'),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _addToCart,
+            child: Text('Ajouter au panier'),
+          ),
         ],
       ),
     );
   }
 }
+
+
+  
--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,23 @@ class MyApp extends StatelessWidget {
   final List<Book> books = [
     Book(
         title: "Livre 1",
-        image: "https://example.com/images/livre1.png",
+        image: "livre.jpg",
         price: 10.99),
     Book(
         title: "Livre 2",
-        image: "https://example.com/images/livre2.png",
+        image: "livre.jpg",
         price: 14.99),
     Book(
         title: "Livre 3",
-        image: "https://example.com/images/livre3.png",
+        image: "livre.jpg",
         price: 9.99),
     Book(
         title: "Livre 4",
-        image: "https://example.com/images/livre4.png",
+        image: "livre.jpg",
         price: 12.99),
     Book(
         title: "Livre 5",
-        image: "https://example.com/images/livre5.png",
+        image: "livre.jpg",
         price: 8.99),
   ];
 
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           itemCount: books.length,
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-              leading: Image.network(books[index].image),
+              leading: Image.asset(books[index].image),
               title: Text(books[index].title),
               trailing: Text("\$${books[index].price}"),
               onTap: () {
@@ -64,3 +64,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
